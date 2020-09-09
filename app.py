7d697c0d3b5eddeb283683040866695c34cdecd6
poudyalanil <anilrunjakote@gmail.com>
2020-09-09 09:23:20 +0545

    Necessary Libs

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ import  pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from matplotlib.cm import rainbow
+
+# Importing libraries for classifiers
+from sklearn.neighbors import  KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+# getting data set
+data = pd.read_csv("heart.csv")
+print(data.head)
