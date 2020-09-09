7f94459f52d04909d29b9cc96ff03db74b451e26
poudyalanil <anilrunjakote@gmail.com>
2020-09-09 11:06:41 +0545

    KNN

--- app.py
+++ app.py
@@ -13,3 +13,17 @@ from sklearn.ensemble import RandomForestClassifier
 # getting data set
 data = pd.read_csv("heart.csv")
 print(data.head)
+print(data.info())
+print(data.describe())
+
+
+# Feature Selection
+import seaborn as sns
+
+# getting correlation
+corr = data.corr()
+top_corr_features = corr.index
+plt.figure(figsize=(20,20))
+
+# getting heatmap
+g = sns.heatmap(data[top_corr_features].corr(),annot=True,cmap="RdYlGn")
